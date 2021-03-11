22294abb29f902582b2f4a58f49b70512b6bbcde
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-03-11 07:43:49 -0600

    Update is_string.js

--- is_string.js
+++ is_string.js
@@ -1,3 +1,5 @@
+/** ver 1.0**/
+
 is_string = function(input) {
   if (Object.prototype.toString.call(input) === '[object String]')
     return true;
@@ -15,3 +17,13 @@ function is_string(input){
   //return recup;
 }
 is_string('test');
+
+/** ver 1.01 **/
+
+function is_string(s){
+  if(typeof(s)==="string")
+    return true;
+  return false;
+}
+is_string([1, 2, 4, 0]);
+is_string('w3resource');
